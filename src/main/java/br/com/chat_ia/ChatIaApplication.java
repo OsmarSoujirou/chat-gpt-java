af9ae60a88b836c03a3b799e65ea6d2fc38e835c
Programador VBA <osmar.lpc.net@gmail.com>
2025-09-07 17:45:53 -0300

    init spring boot project

---
+++ src/main/java/br/com/chat_ia/ChatIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.chat_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chat_ia/ChatIaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chat_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
